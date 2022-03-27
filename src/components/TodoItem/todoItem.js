2cbb24393da9c3a25b32991c90a2a235d7d347e8
yaoyu <yaoyu@yaoyudeMacBook-Air.local>
2022-03-27 23:31:16 +0800

    implement the todoItem component

--- src/components/TodoItem/todoItem.js
+++ src/components/TodoItem/todoItem.js
@@ -1,8 +1,23 @@
+//import style.css
 export function TodoItem(){
+    var todolist = [];
+    const listenEnter = (e)=>{
+        const content = e.target.value;
+        if(e.keyCode === 13){
+        addTodo(content);
+        };
+    };
+    const addTodo = (content)=>{
+        var objTodoItem ={
+        item:"",
+        done:"false"
+        };
+        objTodoItem.item = content;
+        todolist.push(objTodoItem);
+        console.log(todolist);
+    };
     return (
-        <div>
-            This is a TodoItem
-        </div>
+        <input type="text" id="inputTodo" placeholder="添加todo" onKeyDown={listenEnter}/>
     )
 }
 
